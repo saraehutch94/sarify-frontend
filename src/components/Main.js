dd8ea019d648e62f908118175f3c0bbdb70c54df
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-16 14:59:21 -0500

    basic Track component code / import to Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import Home from "../pages/Home";
+import Track from "../pages/Track";
 
 function Main(props) {
   return (
     <div className="main-component">
       <h1>Main Component</h1>
       <Home />
+      <Track />
     </div>
   );
 }
---
+++ src/pages/Track.js
@@ -0,0 +1,5 @@
+function Track(props) {
+  return <h1>Track Component</h1>;
+}
+
+export default Track;
