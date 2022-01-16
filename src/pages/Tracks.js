e787d3e249f71195241b34c73a807797a8b65e5e
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-16 16:15:14 -0500

    map through tracks array in loaded function

--- src/pages/Tracks.js
+++ src/pages/Tracks.js
@@ -1,5 +1,14 @@
 function Tracks(props) {
-  console.log(props.tracks);
+  const tracks = props.tracks;
+
+  const loaded = () => {
+    return tracks.map((track) => (
+      <div key={track._id} className="ind-track">
+        <h2>{track.title}</h2>
+      </div>
+    ));
+  };
+
   return (
     <div className="tracks-component">
       <h1>Tracks Component</h1>
