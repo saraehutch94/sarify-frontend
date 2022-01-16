4b3badf2695e296e980ae4aac0b1195a7077112a
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-16 15:46:08 -0500

    rest of getTracks / setTracks w data

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import Home from "../pages/Home";
 import Tracks from "../pages/Tracks";
 import Track from "../pages/Track";
 import { Route, Switch } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Main(props) {
   const [tracks, setTracks] = useState([]);
@@ -10,9 +10,14 @@ function Main(props) {
   const URL = "https://sarify-backend.herokuapp.com/sarify/tracks";
 
   const getTracks = async () => {
-    const data = await fetch(URL);
-    console.log(data);
+    const response = await fetch(URL);
+    const data = await response.json();
+    setTracks(data);
   };
+
+  useEffect(() => {
+    getTracks();
+  }, []);
 
   return (
     <div className="main-component">
