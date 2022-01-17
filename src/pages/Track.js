9bd4dfa934acf70f8a188fca2f45a58fe4df4229
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-17 14:02:20 -0500

    fix: filter to find

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -1,6 +1,7 @@
 function Track(props) {
   const id = props.match.params.id;
-  const foundTrack = props.tracks.filter((track) => track._id === id);
+  const foundTrack = props.tracks.find((track) => track._id === id);
+  console.log(foundTrack);
   return <h1>Ind. Track Component</h1>;
 }
 
