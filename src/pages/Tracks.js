fc0e06da6825fdb2759c1ca10e3ec4f298a0738b
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-17 13:39:31 -0500

    height and width of track images

--- src/pages/Tracks.js
+++ src/pages/Tracks.js
@@ -4,6 +4,11 @@ function Tracks(props) {
   const loaded = () => {
     return tracks.map((track) => (
       <div key={track._id} className="ind-track">
+        <img
+          src={track.coverArt}
+          alt={track.title}
+          style={{ height: "100px", width: "100px" }}
+        />
         <h2>{track.title}</h2>
       </div>
     ));
