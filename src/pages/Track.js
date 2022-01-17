d3890b5318c88fb91f8f67340d76a133d8ce47f4
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-17 14:08:05 -0500

    render all details of individual track to Track.js

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -1,8 +1,19 @@
 function Track(props) {
   const id = props.match.params.id;
   const foundTrack = props.tracks.find((track) => track._id === id);
-  console.log(foundTrack);
-  return <h1>Ind. Track Component</h1>;
+  return (
+    <div className="ind-track">
+      <img
+        src={foundTrack.coverArt}
+        alt={foundTrack.project}
+        style={{ height: "200px", width: "200px" }}
+      />
+      <h2>{foundTrack.title}</h2>
+      <h3>{foundTrack.artist}</h3>
+      <p>Project: {foundTrack.project}</p>
+      <p>Genre: {foundTrack.genre}</p>
+    </div>
+  );
 }
 
 export default Track;
