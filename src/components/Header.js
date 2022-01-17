ac5b59e23eaf4661cae2894a5d4620819101f668
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-17 11:52:17 -0500

    add Link components to Header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 
 function Header(props) {
-  return <h1>Header Component</h1>;
+  return (
+    <nav>
+      <Link to="/sarify/home">
+        <div className="nav-item">Home</div>
+      </Link>
+      <Link to="/sarify/tracks">
+        <div className="nav-item">Tracks</div>
+      </Link>
+    </nav>
+  );
 }
 
 export default Header;
