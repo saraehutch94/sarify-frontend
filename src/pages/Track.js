9ddc7c4451f3782e1b8fbb9e9ce43281365ad870
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-18 13:25:49 -0500

    add update form to ind Track component

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 function Track(props) {
   const id = props.match.params.id;
   const foundTrack = props.tracks.find((track) => track._id === id);
+
+  const [trackForm, setTrackForm] = useState(foundTrack);
+
   return (
     <div className="ind-track">
       <img
@@ -14,6 +17,55 @@ function Track(props) {
       <h3>{foundTrack.artist}</h3>
       <p>Project: {foundTrack.project}</p>
       <p>Genre: {foundTrack.genre}</p>
+      <h2>Update Track:</h2>
+      <form>
+        <label>
+          <input
+            type="text"
+            name="title"
+            value={trackForm.title}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="artist"
+            value={trackForm.artist}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="coverArt"
+            value={trackForm.coverArt}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="project"
+            value={trackForm.project}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="genre"
+            value={trackForm.genre}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <input type="submit" value="Update Track" />
+      </form>
     </div>
   );
 }
