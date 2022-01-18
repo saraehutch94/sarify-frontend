96db9cd329e8fbb9a17b79a076e78df2a1fefd09
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-18 16:47:23 -0500

    add + comment out code from when refresh did not work

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -107,6 +107,70 @@ function Track(props) {
       {!showPage || !trackForm ? loading() : loaded()}
     </div>
   );
+
+  //   return (
+  //       <div className="ind-track">
+  //         <img
+  //           src={foundTrack.coverArt}
+  //           alt={foundTrack.project}
+  //           style={{ height: "200px", width: "200px" }}
+  //         />
+  //         <h2>{foundTrack.title}</h2>
+  //         <h3>{foundTrack.artist}</h3>
+  //         <p>Project: {foundTrack.project}</p>
+  //         <p>Genre: {foundTrack.genre}</p>
+  //         <button onClick={handleDelete}>Delete Track</button>
+  //         <h2>Update Track:</h2>
+  //         <form onSubmit={handleSubmit}>
+  //           <label>
+  //             <input
+  //               type="text"
+  //               name="title"
+  //               value={trackForm.title}
+  //               onChange={handleChange}
+  //             />
+  //           </label>
+  //           <br />
+  //           <label>
+  //             <input
+  //               type="text"
+  //               name="artist"
+  //               value={trackForm.artist}
+  //               onChange={handleChange}
+  //             />
+  //           </label>
+  //           <br />
+  //           <label>
+  //             <input
+  //               type="text"
+  //               name="coverArt"
+  //               value={trackForm.coverArt}
+  //               onChange={handleChange}
+  //             />
+  //           </label>
+  //           <br />
+  //           <label>
+  //             <input
+  //               type="text"
+  //               name="project"
+  //               value={trackForm.project}
+  //               onChange={handleChange}
+  //             />
+  //           </label>
+  //           <br />
+  //           <label>
+  //             <input
+  //               type="text"
+  //               name="genre"
+  //               value={trackForm.genre}
+  //               onChange={handleChange}
+  //             />
+  //           </label>
+  //           <br />
+  //           <input type="submit" value="Update Track" />
+  //         </form>
+  //       </div>
+  //     );
 }
 
 export default Track;
