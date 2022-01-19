6dedb42ad85cdd11dd18ad4bd99eb07d625c2423
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-19 13:05:22 -0500

    SDK object for firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,3 +3,13 @@ import firebase from "firebase/app";
 
 // Firebase service in scope
 import "firebase/auth";
+
+// SDK
+const firebaseConfig = {
+  apiKey: "AIzaSyBsCiE-a_mkK8e7OyUrkd_sF13ruu5fj4c",
+  authDomain: "react-firebase-projects-5cd63.firebaseapp.com",
+  projectId: "react-firebase-projects-5cd63",
+  storageBucket: "react-firebase-projects-5cd63.appspot.com",
+  messagingSenderId: "283404157023",
+  appId: "1:283404157023:web:66ab88b998849ffc481bea",
+};
