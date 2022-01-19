cca6a8a4045405d59cc811bb340b7dbedb889875
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-19 13:31:54 -0500

    import useState and auth to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { auth } from "./services/firebase";
+
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
