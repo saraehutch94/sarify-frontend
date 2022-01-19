d2f03b59ce5d28c1ab38870264aa2b7cc2ed1212
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-19 18:59:13 -0500

    remove commented code in Track.js

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -5,7 +5,6 @@ function Track(props) {
   const foundTrack = props.tracks.find((track) => track._id === id);
 
   const [trackForm, setTrackForm] = useState(foundTrack);
-  //   const [showPage, setShowPage] = useState();
 
   const handleChange = (e) => {
     setTrackForm({
@@ -17,100 +16,12 @@ function Track(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     props.updateTrack(trackForm, id);
-    // setShowPage(trackForm); // remove later if useEffect API call is not working for page refresh
   };
 
   const handleDelete = () => {
     props.deleteTrack(id);
     props.history.push("/sarify/tracks");
   };
-
-  //   const loading = () => {
-  //     return <h1>Track loading...</h1>;
-  //   };
-
-  //   const loaded = () => {
-  //     return (
-  //       <div className="ind-track">
-  //         <img
-  //           src={showPage.coverArt}
-  //           alt={showPage.project}
-  //           style={{ height: "200px", width: "200px" }}
-  //         />
-  //         <h2>{showPage.title}</h2>
-  //         <h3>{showPage.artist}</h3>
-  //         <p>Project: {showPage.project}</p>
-  //         <p>Genre: {showPage.genre}</p>
-  //         <button onClick={handleDelete}>Delete Track</button>
-  //         <h2>Update Track:</h2>
-  //         <form onSubmit={handleSubmit}>
-  //           <label>
-  //             <input
-  //               type="text"
-  //               name="title"
-  //               value={trackForm.title}
-  //               onChange={handleChange}
-  //             />
-  //           </label>
-  //           <br />
-  //           <label>
-  //             <input
-  //               type="text"
-  //               name="artist"
-  //               value={trackForm.artist}
-  //               onChange={handleChange}
-  //             />
-  //           </label>
-  //           <br />
-  //           <label>
-  //             <input
-  //               type="text"
-  //               name="coverArt"
-  //               value={trackForm.coverArt}
-  //               onChange={handleChange}
-  //             />
-  //           </label>
-  //           <br />
-  //           <label>
-  //             <input
-  //               type="text"
-  //               name="project"
-  //               value={trackForm.project}
-  //               onChange={handleChange}
-  //             />
-  //           </label>
-  //           <br />
-  //           <label>
-  //             <input
-  //               type="text"
-  //               name="genre"
-  //               value={trackForm.genre}
-  //               onChange={handleChange}
-  //             />
-  //           </label>
-  //           <br />
-  //           <input type="submit" value="Update Track" />
-  //         </form>
-  //       </div>
-  //     );
-  //   };
-
-  // remove later if errors occur on deployment
-  //   useEffect(() => {
-  //     async function fetchData() {
-  //       const response = await fetch(props.URL + id);
-  //       const data = await response.json();
-  //       setShowPage(data);
-  //       setTrackForm(data);
-  //     }
-  //     fetchData();
-  //   }, [props.URL, id]);
-
-  //   return (
-  //     <div className="track">
-  //       {!showPage || !trackForm ? loading() : loaded()}
-  //     </div>
-  //   );
 
   return (
     <div className="ind-track">
