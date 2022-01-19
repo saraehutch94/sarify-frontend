1d246de858d8607e51c1f3fe77f7a1de4023a808
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-19 14:35:14 -0500

    put form from Tracks.js into CreateTrack.js

--- src/pages/CreateTrack.js
+++ src/pages/CreateTrack.js
@@ -1,5 +1,63 @@
-function createTrack() {
-  return <h1>Create Track here</h1>;
+function CreateTrack() {
+  return (
+    <div className="create-track">
+      <h2>Create a Track</h2>
+      <h2>Add a Track:</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            name="title"
+            value={formState.title}
+            onChange={handleChange}
+            placeholder="Title"
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="artist"
+            value={formState.artist}
+            onChange={handleChange}
+            placeholder="Artist"
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="coverArt"
+            value={formState.coverArt}
+            onChange={handleChange}
+            placeholder="Cover Art"
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="project"
+            value={formState.project}
+            onChange={handleChange}
+            placeholder="Project/Album"
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="genre"
+            value={formState.genre}
+            onChange={handleChange}
+            placeholder="Genre"
+          />
+        </label>
+        <br />
+        <input type="submit" value="Add Track" />
+      </form>
+    </div>
+  );
 }
 
-export default createTrack;
+export default CreateTrack;
