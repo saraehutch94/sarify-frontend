7987f82ae668c8db45f5b8463c13db8682d16d6e
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-21 11:56:18 -0500

    paste dropdown menu into Dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,17 @@
 function Dropdown() {
-  return <h1>Dropdown Menu</h1>;
+  return (
+    <div className="dropdown-menu">
+      <label htmlFor="genre">Select Genre:</label>
+      <select name="genre" id="genre" onChange={handleChange}>
+        <option select="true" value="All">
+          All Genres
+        </option>
+        <option value="Hip-Hop">Hip-Hop</option>
+        <option value="R&B">R&B</option>
+        <option value="Alternative">Alternative</option>
+      </select>
+    </div>
+  );
 }
 
 export default Dropdown;
