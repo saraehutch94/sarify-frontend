5ef456bea3ce45b3a99280bc3d736c74dc163693
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-22 17:04:22 -0500

    take out colon in Bearer authorization header

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,7 +26,7 @@ function Main(props) {
       method: "POST",
       headers: {
         "Content-Type": "Application/json",
-        Authorization: "Bearer: " + token,
+        Authorization: "Bearer " + token,
       },
       body: JSON.stringify(track),
     });
