a4477af961b95259839fa3d331fc0a72edbf9553
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-25 10:01:32 -0500

    import fonts to index.html

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,39 +3,42 @@ import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
-    <nav>
-      <Link to="/">
-        <img
-          src="https://i.imgur.com/vBc6UJb.png"
-          alt="home-icon"
-          style={{ height: "25px", width: "25px" }}
-        />
-      </Link>
-      <Link to="/sarify/tracks">
-        <img
-          src="https://i.imgur.com/72xhKjz.png"
-          alt="tracks-icon"
-          style={{ height: "25px", width: "25px" }}
-        />
-      </Link>
-      {props.user ? (
-        <input
-          type="image"
-          src="https://i.imgur.com/NVAHAOs.png"
-          alt="logout-icon"
-          style={{ height: "25px", width: "25px" }}
-          onClick={logout}
-        />
-      ) : (
-        <input
-          type="image"
-          src="https://i.imgur.com/IIUGvUg.png"
-          alt="login-icon"
-          style={{ height: "25px", width: "25px" }}
-          onClick={login}
-        />
-      )}
-    </nav>
+    <header>
+      <div className="title">Sarify</div>
+      <nav>
+        <Link to="/">
+          <img
+            src="https://i.imgur.com/vBc6UJb.png"
+            alt="home-icon"
+            style={{ height: "25px", width: "25px" }}
+          />
+        </Link>
+        <Link to="/sarify/tracks">
+          <img
+            src="https://i.imgur.com/72xhKjz.png"
+            alt="tracks-icon"
+            style={{ height: "25px", width: "25px" }}
+          />
+        </Link>
+        {props.user ? (
+          <input
+            type="image"
+            src="https://i.imgur.com/NVAHAOs.png"
+            alt="logout-icon"
+            style={{ height: "25px", width: "25px" }}
+            onClick={logout}
+          />
+        ) : (
+          <input
+            type="image"
+            src="https://i.imgur.com/IIUGvUg.png"
+            alt="login-icon"
+            style={{ height: "25px", width: "25px" }}
+            onClick={login}
+          />
+        )}
+      </nav>
+    </header>
   );
 }
 
