5b96e7b23254f2e33556db03c1c14fd056c7f463
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-25 14:53:42 -0500

    add underline to sarify on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,29 +6,24 @@ function Home(props) {
   return (
     <div className="home-page">
       <div className="home-page-content">
-        <div classNam="content-wrapper">
-          <h1>sarify</h1>
-          <div className="home-nav">
-            <Link to="/sarify/tracks">
-              <div className="music-icon"></div>
-            </Link>
-            {props.user ? (
-              <div className="google-logout-icon" onClick={logout}></div>
-            ) : (
-              <div className="google-login-icon" onClick={login}></div>
-            )}
-            <a
-              href="https://github.com/saraehutch94/sarify-frontend"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://i.imgur.com/X20Qz9B.png"
-                alt="github-watermark"
-                style={{ height: "25px", width: "25px" }}
-              />
-            </a>
-          </div>
+        <h1>sarify</h1>
+        <div className="underline"></div>
+        <div className="home-nav">
+          <Link to="/sarify/tracks">
+            <div className="music-icon"></div>
+          </Link>
+          {props.user ? (
+            <div className="google-logout-icon" onClick={logout}></div>
+          ) : (
+            <div className="google-login-icon" onClick={login}></div>
+          )}
+          <a
+            href="https://github.com/saraehutch94/sarify-frontend"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="github-icon"></div>
+          </a>
         </div>
       </div>
     </div>
