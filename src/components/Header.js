ad27bf83b1c9bfe5225ad149ed04506faea6a110
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-25 19:47:35 -0500

    set up icons and hover status of header nav icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,58 +9,22 @@ function Header(props) {
       <div className="underline-header"></div>
       <nav className="nav-icons">
         <Link to="/">
-          <img
-            src="https://i.imgur.com/vBc6UJb.png"
-            alt="home-icon"
-            style={{
-              height: "25px",
-              width: "25px",
-              filter: "brightness(0) invert(1)",
-            }}
-          />
+          <div className="home-icon"></div>
         </Link>
         <Link to="/sarify/tracks">
-          <img
-            src="https://i.imgur.com/72xhKjz.png"
-            alt="tracks-icon"
-            style={{
-              height: "25px",
-              width: "25px",
-              filter: "brightness(0) invert(1)",
-            }}
-          />
+          <div className="header-music-icon"></div>
         </Link>
         {props.user ? (
-          <input
-            type="image"
-            src="https://i.imgur.com/NVAHAOs.png"
-            alt="logout-icon"
-            style={{ height: "25px", width: "25px" }}
-            onClick={logout}
-          />
+          <div className="header-logout-icon" onClick={logout}></div>
         ) : (
-          <input
-            type="image"
-            src="https://i.imgur.com/IIUGvUg.png"
-            alt="login-icon"
-            style={{
-              height: "25px",
-              width: "25px",
-              filter: "brightness(0) invert(1)",
-            }}
-            onClick={login}
-          />
+          <div className="header-login-icon" onClick={login}></div>
         )}
         <a
           href="https://github.com/saraehutch94/sarify-frontend"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            src="https://i.imgur.com/X20Qz9B.png"
-            alt="github-watermark"
-            style={{ height: "25px", width: "25px" }}
-          />
+          <div className="header-github-icon"></div>
         </a>
       </nav>
     </header>
