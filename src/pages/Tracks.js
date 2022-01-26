48968ca82f1e555348f3ff01115cc33c9f7d79b7
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-25 19:16:45 -0500

    start styling tracks page

--- src/pages/Tracks.js
+++ src/pages/Tracks.js
@@ -13,30 +13,30 @@ function Tracks(props) {
 
   const allLoaded = () => {
     return (
-      <div className="tracks">
-        <div className="contribute-link">
+      <div className="tracks-wrapper">
+        <div className="tracks">
           <Link to="/sarify/create">
             <button disabled={!props.user}>Contribute</button>
           </Link>
-        </div>
 
-        <Dropdown setGenre={setGenre} />
+          <Dropdown setGenre={setGenre} />
 
-        <div className="all-tracks">
-          {tracks.map((track) => {
-            return (
-              <div key={track._id} className="ind-track">
-                <img
-                  src={track.coverArt}
-                  alt={track.title}
-                  style={{ height: "200px", width: "200px" }}
-                />
-                <Link to={`/sarify/tracks/${track._id}`}>
-                  <h2>{track.title}</h2>
-                </Link>
-              </div>
-            );
-          })}
+          <div className="all-tracks">
+            {tracks.map((track) => {
+              return (
+                <div key={track._id} className="ind-track">
+                  <img
+                    src={track.coverArt}
+                    alt={track.title}
+                    style={{ height: "200px", width: "200px" }}
+                  />
+                  <Link to={`/sarify/tracks/${track._id}`}>
+                    <h2>{track.title}</h2>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     );
@@ -44,30 +44,30 @@ function Tracks(props) {
 
   const genreLoaded = () => {
     return (
-      <div className="tracks">
-        <div className="contribute-link">
+      <div className="tracks-wrapper">
+        <div className="tracks">
           <Link to="/sarify/create">
             <button disabled={!props.user}>Contribute</button>
           </Link>
-        </div>
 
-        <Dropdown setGenre={setGenre} />
+          <Dropdown setGenre={setGenre} />
 
-        <div className="all-tracks">
-          {tracks
-            .filter((t) => t.genre === trackState)
-            .map((filteredTrack) => (
-              <div key={filteredTrack._id} className="ind-track">
-                <img
-                  src={filteredTrack.coverArt}
-                  alt={filteredTrack.title}
-                  style={{ height: "200px", width: "200px" }}
-                />
-                <Link to={`/sarify/tracks/${filteredTrack._id}`}>
-                  <h2>{filteredTrack.title}</h2>
-                </Link>
-              </div>
-            ))}
+          <div className="all-tracks">
+            {tracks
+              .filter((t) => t.genre === trackState)
+              .map((filteredTrack) => (
+                <div key={filteredTrack._id} className="ind-track">
+                  <img
+                    src={filteredTrack.coverArt}
+                    alt={filteredTrack.title}
+                    style={{ height: "200px", width: "200px" }}
+                  />
+                  <Link to={`/sarify/tracks/${filteredTrack._id}`}>
+                    <h2>{filteredTrack.title}</h2>
+                  </Link>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     );
