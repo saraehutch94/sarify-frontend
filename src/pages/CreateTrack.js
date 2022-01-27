42ac7a2d90f6870d553d528a9c5864e803a51ba0
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-27 12:06:13 -0500

    continue styling create track page

--- src/pages/CreateTrack.js
+++ src/pages/CreateTrack.js
@@ -33,104 +33,108 @@ function CreateTrack(props) {
 
   return (
     <div className="create-track">
-      <div className="create-track-title">Create a Track</div>
-      {["jpg", "jpeg", "png"].some((files) =>
-        formState.coverArt.includes(files)
-      ) ? (
-        <div
-          className="create-coverArt"
-          style={{
-            background: `url(${formState.coverArt})`,
-            backgroundSize: "cover",
-            height: "200px",
-            width: "200px",
-          }}
-        ></div>
-      ) : (
-        <div
-          className="create-coverArt"
-          style={{
-            color: "white",
-            background: "black",
-            height: "200px",
-            width: "200px",
-            borderRadius: "10%",
-          }}
-        >
-          Cover Art will appear here
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <label>
+      <div className="create-track-title">add track</div>
+      <div className="image-and-form">
+        {["jpg", "jpeg", "png"].some((files) =>
+          formState.coverArt.includes(files)
+        ) ? (
+          <div
+            className="create-coverArt"
+            style={{
+              background: `url(${formState.coverArt})`,
+              backgroundSize: "cover",
+              height: "250px",
+              width: "250px",
+            }}
+          ></div>
+        ) : (
+          <div
+            className="create-coverArt"
+            style={{
+              color: "white",
+              background: "black",
+              height: "250px",
+              width: "250px",
+              borderRadius: "10%",
+            }}
+          >
+            <div className="image-file">jpg</div>
+            <div className="image-file">jpeg</div>
+            <div className="image-file">png</div>
+          </div>
+        )}
+        <form className="create-form" onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="text"
+              name="title"
+              value={formState.title}
+              onChange={handleChange}
+              placeholder="Title"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="artist"
+              value={formState.artist}
+              onChange={handleChange}
+              placeholder="Artist"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="coverArt"
+              value={formState.coverArt}
+              onChange={handleChange}
+              placeholder="Cover Art"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="project"
+              value={formState.project}
+              onChange={handleChange}
+              placeholder="Project/Album"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="genre"
+              value={formState.genre}
+              onChange={handleChange}
+              placeholder="Genre"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="duration"
+              value={formState.duration}
+              onChange={handleChange}
+              placeholder="Duration"
+            />
+          </label>
+          <br />
           <input
-            type="text"
-            name="title"
-            value={formState.title}
-            onChange={handleChange}
-            placeholder="Title"
+            disabled={
+              !["jpg", "jpeg", "png"].some((files) =>
+                formState.coverArt.includes(files)
+              )
+            }
+            type="submit"
+            value="Add Track"
           />
-        </label>
-        <br />
-        <label>
-          <input
-            type="text"
-            name="artist"
-            value={formState.artist}
-            onChange={handleChange}
-            placeholder="Artist"
-          />
-        </label>
-        <br />
-        <label>
-          <input
-            type="text"
-            name="coverArt"
-            value={formState.coverArt}
-            onChange={handleChange}
-            placeholder="Cover Art"
-          />
-        </label>
-        <br />
-        <label>
-          <input
-            type="text"
-            name="project"
-            value={formState.project}
-            onChange={handleChange}
-            placeholder="Project/Album"
-          />
-        </label>
-        <br />
-        <label>
-          <input
-            type="text"
-            name="genre"
-            value={formState.genre}
-            onChange={handleChange}
-            placeholder="Genre"
-          />
-        </label>
-        <br />
-        <label>
-          <input
-            type="text"
-            name="duration"
-            value={formState.duration}
-            onChange={handleChange}
-            placeholder="Duration"
-          />
-        </label>
-        <br />
-        <input
-          disabled={
-            !["jpg", "jpeg", "png"].some((files) =>
-              formState.coverArt.includes(files)
-            )
-          }
-          type="submit"
-          value="Add Track"
-        />
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
