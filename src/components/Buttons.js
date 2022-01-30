4c8023003550417562452a048cc5afa0eb4ff509
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-30 12:36:07 -0500

    userButtons and disabledButtons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,3 +1,35 @@
-function Buttons(props) {}
+function Buttons(props) {
+  const userButtons = () => {
+    return (
+      <div className="button-flex">
+        <button
+          className="track-button-user update-button"
+          onClick={handleForm}
+        >
+          Update Track
+        </button>
+        <button className="track-button-user" onClick={handleDelete}>
+          Delete Track
+        </button>
+      </div>
+    );
+  };
+
+  const disabledButtons = () => {
+    return (
+      <div className="button-flex">
+        <button
+          className="track-button-disable update-button"
+          disabled={!props.user}
+        >
+          Update Track
+        </button>
+        <button className="track-button-disable" disabled={!props.user}>
+          Delete Track
+        </button>
+      </div>
+    );
+  };
+}
 
 export default Buttons;
