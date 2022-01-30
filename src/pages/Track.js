007515deafc8e9076dd03ba9d6d38379dfc49458
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-30 12:52:06 -0500

    remove commented out old buttons from Track.js

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -41,31 +41,6 @@ function Track(props) {
               id={id}
               history={props.history}
             />
-            {/* {props.user ? (
-              <div className="button-flex">
-                <button
-                  className="track-button-user update-button"
-                  onClick={handleForm}
-                >
-                  Update Track
-                </button>
-                <button className="track-button-user" onClick={handleDelete}>
-                  Delete Track
-                </button>
-              </div>
-            ) : (
-              <div className="button-flex">
-                <button
-                  className="track-button-disable update-button"
-                  disabled={!props.user}
-                >
-                  Update Track
-                </button>
-                <button className="track-button-disable" disabled={!props.user}>
-                  Delete Track
-                </button>
-              </div>
-            )} */}
           </div>
         )}
       </div>
