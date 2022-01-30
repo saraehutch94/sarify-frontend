b07cf4def27c979d7e1e454546cea9fd2ae63f81
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-30 11:16:56 -0500

    add form code to updateFormDisable / change classNames

--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -97,7 +97,86 @@ function UpdateForm(props) {
     );
   };
 
-  const updateFormDisable = () => {};
+  const updateFormDisable = () => {
+    return (
+      <div className="track-details">
+        <form className="update-form" onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="text"
+              name="title"
+              value={trackForm.title}
+              onChange={handleChange}
+              disabled={!props.user}
+              className="update-input-disable"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="artist"
+              value={trackForm.artist}
+              onChange={handleChange}
+              disabled={!props.user}
+              className="update-input-disable"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="coverArt"
+              value={trackForm.coverArt}
+              onChange={handleChange}
+              disabled={!props.user}
+              className="update-input-disable"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="project"
+              value={trackForm.project}
+              onChange={handleChange}
+              disabled={!props.user}
+              className="update-input-disable"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="genre"
+              value={trackForm.genre}
+              onChange={handleChange}
+              disabled={!props.user}
+              className="update-input-disable"
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="duration"
+              value={trackForm.duration}
+              onChange={handleChange}
+              disabled={!props.user}
+              className="update-input-disable"
+            />
+          </label>
+          <br />
+          <input
+            type="submit"
+            value="Submit Changes"
+            className="update-submit-disable"
+            disabled={!props.user}
+          />
+        </form>
+      </div>
+    );
+  };
 }
 
 export default UpdateForm;
