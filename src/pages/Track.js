75e0e63048aa85bfc1637b17a333a4c315b10e2f
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-30 10:57:40 -0500

    pass props down to UpdateForm

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -42,7 +42,11 @@ function Track(props) {
           />
         </div>
         {update ? (
-          <UpdateForm />
+          <UpdateForm
+            user={props.user}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+          />
         ) : (
           <div className="track-details">
             <h2 className="ind-track-title">{foundTrack.title}</h2>
